2497b2294c43301712de5603d32907e623a182f1
Severin Leander <gasserseverin1@gmail.com>
2025-02-18 17:09:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,24 +4,24 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-// Statische Dateien aus dem "public"-Verzeichnis bereitstellen
+// Statische Dateien aus dem "public"-Ordner bereitstellen
 app.use(express.static('public'));
 
-// Socket.IO-Verbindung
+// Socket.IO: Verbindung und Nachrichten-Handling
 io.on('connection', (socket) => {
-  console.log('Ein Nutzer hat sich verbunden.');
+  console.log('Ein Nutzer verbunden: ' + socket.id);
 
-  // Wenn eine Nachricht empfangen wird, an alle Clients senden
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on('chat message', (data) => {
+    // Leite das Nachrichtenobjekt (enthält senderId, message und timestamp) an alle Clients weiter
+    io.emit('chat message', data);
   });
 
   socket.on('disconnect', () => {
-    console.log('Ein Nutzer hat die Verbindung getrennt.');
+    console.log('Nutzer getrennt: ' + socket.id);
   });
 });
 
-// Server startet auf Port 3000
+// Server startet auf Port 3000 oder dem in der Umgebungsvariable angegebenen Port
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log(`Server läuft auf Port ${PORT}`);
