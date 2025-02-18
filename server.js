9f761788fdf65a2f95bd8d00050deeed68fcf406
Severin Leander <gasserseverin1@gmail.com>
2025-02-18 20:22:04 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+// Statische Dateien aus dem "public"-Verzeichnis bereitstellen
+app.use(express.static('public'));
+
+// Socket.IO-Verbindung
+io.on('connection', (socket) => {
+  console.log('Ein Nutzer hat sich verbunden.');
+
+  // Wenn eine Nachricht empfangen wird, an alle Clients senden
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Ein Nutzer hat die Verbindung getrennt.');
+  });
+});
+
+// Server startet auf Port 5500
+const PORT = process.env.PORT || 5500;
+http.listen(PORT, () => {
+  console.log(`Server läuft auf Port ${PORT}`);
+});
